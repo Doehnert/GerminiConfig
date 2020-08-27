d07546f48550cfc68270d3f3122375d2cbaee662
eltonfd@gmail.com <eltonfd@gmail.com>
2020-08-27 15:51:45 -0300

    Novidades

---
+++ Block/System/Config/Form/Field/Symbol.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Vexpro\GerminiConfig\Block\System\Config\Form\Field;
+
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Symbol extends \Magento\Config\Block\System\Config\Form\Field
+{
+  protected function _getElementHtml(AbstractElement $element)
+  {
+    $element->setDisabled('disabled');
+    return $element->getElementHtml();
+  }
+}
