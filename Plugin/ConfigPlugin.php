bbdf0f18743c68fee5a3a470f76ce98ce64e3306
eltonfd@gmail.com <eltonfd@gmail.com>
2020-11-27 17:37:21 -0300

    teste

--- Plugin/ConfigPlugin.php
+++ Plugin/ConfigPlugin.php
@@ -43,61 +43,55 @@ class ConfigPlugin
             $login = $subject->get()['groups']['general']['fields']['identity_login']['value'];
         } catch (Exception $e) {
             return $proceed();
-        } finally {
-
-            // $messageManager->addSuccess($url_base);
-
-        if (!isset($login))
-            return $proceed();
-
-        $password = $subject->get()['groups']['general']['fields']['identity_password']['value'];
-
-        $url_base = $subject->get()['groups']['general']['fields']['identity_url']['value'];
-
-        // $url_base = $this->scopeConfig->getValue('acessos/general/identity_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-
-        $response = "";
-        $url = $url_base . '/connect/token';
-        $params = [
-            "username" => $login,
-            "password" => $password,
-            "client_id" => "ro.client.partner",
-            "client_secret" => "secret",
-            "grant_type" => "password",
-            "scope" => "germini-api openid profile"
-        ];
-
-        $data_json = json_encode($params);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: text/plain'));
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $response  = curl_exec($ch);
-        // $this->_curl->post($url, $params);
-        //response will contain the output in form of JSON string
-        $resultado = json_decode($response);
-
-        $messageManager->addSuccess($resultado);
-
-
-
-        if ($response == "") {
-            $messageManager->addError('Usuário não existe no Germini');
-            return;
-        } else {
-            if (isset($resultado->error)) {
-                $messageManager->addError('Erro ao conectar com germini');
-                return;
-            } else {
-                $token = json_decode($response)->access_token;
-                 // Salva o token em uma variável de sessão
-                $this->catalogSession->setData('token', $token);
-                $messageManager->addSuccess('Usuário e senha validados com sucesso');
-            }
         }
+        finally
+        {
+
+            if (!isset($login))
+                return $proceed();
+
+            $password = $subject->get()['groups']['general']['fields']['identity_password']['value'];
+
+            $url_base = $subject->get()['groups']['general']['fields']['identity_url']['value'];
+
+            // $url_base = $this->scopeConfig->getValue('acessos/general/identity_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+
+            $response = "";
+            $url = $url_base . '/connect/token';
+            $params = [
+                "username" => $login,
+                "password" => $password,
+                "client_id" => "ro.client.partner",
+                "client_secret" => "secret",
+                "grant_type" => "password",
+                "scope" => "germini-api openid profile"
+            ];
+
+            $data_json = json_encode($params);
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: text/plain'));
+
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            $response  = curl_exec($ch);
+            // $this->_curl->post($url, $params);
+            //response will contain the output in form of JSON string
+            $resultado = json_decode($response);
+
+            $messageManager->addSuccess($response);
+
+
+            if ($response == "" || $resultado->error){
+                $messageManager->addError('Erro ao conectar com germini ou usuário e senha incorretos');
+                return;
+            }
+
+            $token = json_decode($response)->access_token;
+            // Salva o token em uma variável de sessão
+            $this->catalogSession->setData('token', $token);
+            $messageManager->addSuccess('Usuário e senha validados com sucesso');
         }
 
         return $proceed();
